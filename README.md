f7a418aae93de0af75f93b10efc276c300f2af19
geremachek <jonahgrongstad@gmail.com>
2022-07-11 17:27:21 -0700

    docs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Lilith
 
 A really simple Emacs configuration, near stock.
+
+## Features
+
+* Acme colorscheme
+* No GUI frils
+* Some elementary keybindings
+* Nothing else!
--- init.el
+++ init.el
@@ -45,3 +45,41 @@
 ;; Welcome to Lilith
 
 ")
+
+;; Key Bindings
+
+;; Opening things
+
+;; Start a new bash session
+
+(defun bash-session ()
+  (interactive)
+  (term "/bin/bash"))
+
+(global-set-key (kbd "C-c t") 'bash-session) ;; Open a new terminal session
+
+;; Common tasks
+
+(global-set-key (kbd "C-c e") 'eval-buffer) ;; Evaluate the buffer
+
+;; The split the window and focus the right split
+
+(defun focused-right ()
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+
+(global-set-key (kbd "C-c s") 'focused-right)
+
+;; Open a terminal in a split
+
+(defun split-term ()
+  (interactive)
+  (focused-right)
+  (bash-session))
+
+(global-set-key (kbd "C-c y") 'split-term)
+
+;; Kill and close
+
+(global-set-key (kbd "C-c k") 'kill-buffer-and-window)
